6452d94e629ae381658ef36781968e0ad77f1e37
Tran Huu Tri <tranhuutri0909@gmail.com>
2023-01-05 11:13:07 +0700

    + Add boss behaviour

--- Assets/Scripts/Devils/BasicBoss.cs
+++ Assets/Scripts/Devils/BasicBoss.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class BasicBoss : MonoBehaviour
 {
+    [SerializeField] private GameObject attackWave;
+    [SerializeField] private Transform rightHand;
+    [SerializeField] private Transform leftHand;
     private float attackCountdown;
     private const float ATTACK_COOLDOWN = 10f;
     private Animator anim;
+    private const int MAX_ATTACK_STYLE = 4;
+    private readonly string[] ATTACK_STYLE_STR = new string[] {
+        "LeftHandAttack", "RightHandAttack", "LeftHandSideAttack", "RightHandSideAttack"};
+
+    [SerializeField] private int cheatBossAttackStyle = -1;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,30 +35,38 @@ public class BasicBoss : MonoBehaviour
     }
     private void Attack()
     {
-        int rand = Random.Range(0, 2);
-        if (rand < 1)
-        {
-            anim.Play("LeftHandAttack");
-        }
-        else
-            anim.Play("RightHandAttack");
+        int attackStyle = Random.Range(0, MAX_ATTACK_STYLE);
 
+        if (cheatBossAttackStyle != -1)
+            attackStyle = Mathf.Clamp(attackStyle, 0, MAX_ATTACK_STYLE - 1);
+            
+        anim.Play(ATTACK_STYLE_STR[attackStyle]);
         attackCountdown = ATTACK_COOLDOWN;
     }
     public void SpawnAttackLeftDown()
     {
-
+        SpawnAttackWave(leftHand, true);
     }
     public void SpawnAttackRightDown()
     {
-        
+        SpawnAttackWave(rightHand, true);
     }
     public void SpawnAttackLeftSide()
     {
-        
+        SpawnAttackWave(leftHand, false);
     }
     public void SpawnAttackRightSide()
     {
-        
+        SpawnAttackWave(rightHand, false, true);
+    }
+    public void SpawnAttackWave(Transform hand, bool isHorizonDir, bool flipY = false)
+    {
+        float horizonDir = isHorizonDir ? 0f : 90f;
+        GameObject wave = Instantiate(attackWave, hand.position, Quaternion.Euler(0f, 0f, horizonDir));
+
+        if (flipY)
+        {
+            wave.transform.localScale = new Vector3(1f, -1f, 1f);
+        }
     }
 }
