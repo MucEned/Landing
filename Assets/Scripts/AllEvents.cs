e9d3b10235c2f00c0876b9948253b31134015878
htdat2000 <12a6.ht.dat@gmail.com>
2023-01-24 10:28:11 +0700

    Rearranging AllEvents

--- Assets/Scripts/AllEvents.cs
+++ Assets/Scripts/AllEvents.cs
@@ -7,22 +7,28 @@ namespace GameEvents
 {
     public class AllEvents
     {
+        [Header("Player")]
         public static Action OnPlayerJump;
         public static Action OnPlayerLanding;
         public static Action OnPlayerTouchTheGround;
+        public static Action OnPlayerDead;
+        public static Action OnLandOnBoss;
+
+        [Header("Manager")]
         public static Action<bool> OnBossingPhase;
         public static Action OnDevilDead;
         public static Action<int> OnPlayerActionPointSet;
         public static Action OnGamePause; //call it to pause or resume the game
-        public static Action OnScoreUpdate; //call whenever player get score 
         public static Action<int> OnStreak;
         public static Action OnResetStreak;
         public static Action<int> OnWorldDeviated;
-        public static Action OnPlayerDead;
         public static Action<float, float> OnTimeScale;
-        public static Action OnLandOnBoss;
+        
+        [Header("UI")]
+        public static Action OnScoreUpdate; //call whenever player get score 
         public static Action OnTimerUpdate; //used to update the timer text UI
-        public static Action OnMusicSettingChange; 
+        public static Action OnMusicSettingChange;
         public static Action OnSFXSettingChange;
+
     }
 }
