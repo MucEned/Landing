8e9c20c0acf27b6a3413c77be7af447818ecc274
htdat2000 <12a6.ht.dat@gmail.com>
2023-02-16 20:30:27 +0700

    AlertSign anim

--- Assets/Scripts/Utils/AlertSign.cs
+++ Assets/Scripts/Utils/AlertSign.cs
@@ -1,36 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class AlertSign : MonoBehaviour
 {
+    [SerializeField] private float shakeScaleDuration = 0.25f;
+    [SerializeField] private float shakeScaleStrength = 0.25f;
     [SerializeField] private const float TRIGGER_TIME = 3;
+    private const float ANIM_TIME = 0.3f;
     private float triggerTimeCountdown;
-    
+    private float animCountdown;
+
     [SerializeField] GameObject objectToTrigger;
+
+    private bool isScaleUp = false;
 
     private void OnEnable()
     {
         triggerTimeCountdown = TRIGGER_TIME;
-    } 
+    }
     private void Update()
     {
         CheckToActiveObject();
+        CheckToPlayAnim();
     }
     private void CheckToActiveObject()
     {
-        if(triggerTimeCountdown >= 0)
+        if (triggerTimeCountdown >= 0)
         {
             triggerTimeCountdown -= Time.deltaTime;
-            if(triggerTimeCountdown < 0)
+            if (triggerTimeCountdown < 0)
             {
                 objectToTrigger.SetActive(true);
                 this.gameObject.SetActive(false);
             }
         }
     }
+    private void CheckToPlayAnim()
+    {
+        if (animCountdown >= 0)
+        {
+            animCountdown -= Time.deltaTime;
+            if (animCountdown < 0)
+            {
+                PlayAnim();
+            }
+        }
+    }
+
     private void PlayAnim()
     {
-
+        transform.DOShakeScale(shakeScaleDuration, shakeScaleStrength);
+        animCountdown = ANIM_TIME;
     }
 }
